72d21ddcc4ca46ff7a2fe75d2c849894c8f46b44
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-06-18 10:24:31 +0200

    LMB-559: update message when sending a 200

--- routes/burgemeester-benoeming.ts
+++ routes/burgemeester-benoeming.ts
@@ -520,7 +520,9 @@ const onBurgemeesterBenoeming = async (req: Request, res: Response) => {
   try {
     await checkAuthorization(req);
     await onBurgemeesterBenoemingSafe(req);
-    res.status(200).send({ message: 'File uploaded' });
+    res
+      .status(200)
+      .send({ message: `Burgemeester-benoeming: ${req.body.status}` });
   } catch (e) {
     const status = e.status || 500;
     res.status(status).send({ error: e.message });
