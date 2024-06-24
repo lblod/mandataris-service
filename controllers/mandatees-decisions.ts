0f8d4688cc8e9b5b3f8ac8f125be6ca5bea9625e
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-06-24 16:39:44 +0200

    LMB-519: check what values that are missing in the target

--- controllers/mandatees-decisions.ts
+++ controllers/mandatees-decisions.ts
@@ -25,4 +25,42 @@ export async function getDifferencesForTriples(changeSets: Array<Changeset>) {
     '|> Target values for subject and predicate:',
     quadsInTarget.length,
   );
+  matchConsumedWithTargetQuads(quadsForSubjects, quadsInTarget);
+}
+
+function matchConsumedWithTargetQuads(
+  consumedQuads: Array<Quad>,
+  quadsInTarget: Array<Quad>,
+): void {
+  if (quadsInTarget.length === 0) {
+    console.log('|> None of the quads exist in the target');
+    return;
+  }
+
+  for (const consumed of consumedQuads) {
+    const foundInCurrent = quadsInTarget.find(
+      (quad: Quad) =>
+        quad.subject.value == consumed.subject.value &&
+        quad.predicate.value == consumed.predicate.value,
+    );
+
+    if (!foundInCurrent) {
+      console.log(
+        `|> Predicate ${consumed.predicate.value} is missing in current quads for subject: ${consumed.subject.value}`,
+      );
+      return;
+    }
+
+    if (foundInCurrent.object.value !== consumed.object.value) {
+      console.log(
+        `|> We a found a difference in value for predicate: ${consumed.predicate.value}: \n \t old value = ${foundInCurrent.object.value} \n \t new value = ${consumed.object.value}`,
+      );
+    }
+
+    console.log(
+      `|> No Difference between incoming an current data for predicate ${consumed.predicate.value}`,
+    );
+  }
+  console.log('|> Matched triples with current and incoming');
+  console.log('|> --------------------------');
 }
