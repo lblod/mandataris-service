1c3d46f7ae5ea4cdcafe80ff308db6588a606a7b
Ben Fidlers <ben.fidlers@qmino.com>
2024-07-08 12:56:59 +0200

    move some logic

--- routes/burgemeester-benoeming.ts
+++ routes/burgemeester-benoeming.ts
@@ -1,31 +1,11 @@
 import multer from 'multer';
-import { Request, Response } from 'express';
-
 import Router from 'express-promise-router';
-import { checkAuthorization } from '../data-access/burgemeester';
-import { onBurgemeesterBenoemingSafe } from '../controllers/burgemeester-benoeming';
+import { onBurgemeesterBenoeming } from '../controllers/burgemeester-benoeming';
 
 const burgemeesterRouter = Router();
 
 const upload = multer({ dest: '/uploads/' });
 
-burgemeesterRouter.post(
-  '/',
-  upload.single('file'),
-  async (req: Request, res: Response) => {
-    try {
-      await checkAuthorization(req);
-      await onBurgemeesterBenoemingSafe(req);
-      res
-        .status(200)
-        .send({ message: `Burgemeester-benoeming: ${req.body.status}` });
-    } catch (e) {
-      const status = e.status || 500;
-      res.status(status).send({ error: e.message });
-      console.error(`[${status}]: ${e.message}`);
-      console.error(e.stack);
-    }
-  },
-);
+burgemeesterRouter.post('/', upload.single('file'), onBurgemeesterBenoeming);
 
 export { burgemeesterRouter };
