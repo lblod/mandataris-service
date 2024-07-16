66f5e6e0b1179a8fd7be730e51684e6fe6519bc3
Ben Fidlers <ben.fidlers@qmino.com>
2024-07-16 13:48:10 +0200

    exclude history graphs

--- data-access/mandaat.ts
+++ data-access/mandaat.ts
@@ -9,25 +9,30 @@ export const getNbActiveMandatarissen = async (mandaatId: string) => {
   PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
 
   SELECT (COUNT(DISTINCT ?mandataris) as ?count) WHERE {
-    ?mandaat mu:uuid ${sparqlEscapeString(mandaatId)};
-        ^org:hasPost ?orgaanInTijd.
-    ?mandataris a mandaat:Mandataris;
-        org:holds ?mandaat;
-        mandaat:isBestuurlijkeAliasVan ?persoon.
-    OPTIONAL {
-      ?mandataris mandaat:status ?status.
-    }
-    OPTIONAL {
-      ?mandataris mandaat:einde ?mandatarisEinde.
-    }
-    OPTIONAL {
-      ?orgaanInTijd mandaat:bindingEinde ?orgaanEinde.
+    GRAPH ?g {
+      ?mandaat mu:uuid ${sparqlEscapeString(mandaatId)};
+          ^org:hasPost ?orgaanInTijd.
+      ?mandataris a mandaat:Mandataris;
+          org:holds ?mandaat;
+          mandaat:isBestuurlijkeAliasVan ?persoon.
+      OPTIONAL {
+        ?mandataris mandaat:status ?status.
+      }
+      OPTIONAL {
+        ?mandataris mandaat:einde ?mandatarisEinde.
+      }
+      OPTIONAL {
+        ?orgaanInTijd mandaat:bindingEinde ?orgaanEinde.
+      }
     }
     BIND(IF(BOUND(?orgaanEinde), ?orgaanEinde, ${now}) AS ?actualOrgaanEinde).
     BIND(IF(?actualOrgaanEinde <= ${now}, ?actualOrgaanEinde, ${now}) AS ?testEinde).
     FILTER(!BOUND(?mandatarisEinde) || ?mandatarisEinde >= ?testEinde).
     # Filter mandatarissen that are either effectief, verhinderd or titelvoerend (or have no status)
     FILTER(!BOUND(?status) || ?status IN (<http://data.vlaanderen.be/id/concept/MandatarisStatusCode/21063a5b-912c-4241-841c-cc7fb3c73e75>, <http://data.vlaanderen.be/id/concept/MandatarisStatusCode/c301248f-0199-45ca-b3e5-4c596731d5fe>, <http://data.vlaanderen.be/id/concept/MandatarisStatusCode/aacb3fed-b51d-4e0b-a411-f3fa641da1b3>)).
+    FILTER NOT EXISTS {
+      ?g a <http://mu.semte.ch/vocabularies/ext/FormHistory>
+    }
   }
   `;
 
