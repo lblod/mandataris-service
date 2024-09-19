7558cc76d3a33784b59e22ac0684432064f45796
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-09-19 15:00:16 +0200

    LMB-557: get contact email from bestuurseenheid

--- data-access/bestuurseenheid.ts
+++ data-access/bestuurseenheid.ts
@@ -3,6 +3,10 @@ import { findFirstSparqlResult } from '../util/sparql-result';
 import { Term } from '../types';
 import { querySudo } from '@lblod/mu-auth-sudo';
 
+export const bestuurseenheid_sudo = {
+  getContactEmailFromMandataris,
+};
+
 export async function findBestuurseenheidForMandaat(
   mandaat: Term,
 ): Promise<Term | null> {
@@ -37,4 +41,35 @@ export async function findBestuurseenheidForMandaat(
       result.id.value +
       '/LoketLB-mandaatGebruiker',
   } as Term;
+}
+
+async function getContactEmailFromMandataris(mandatarisUri: Term) {
+  const query = `
+    PREFIX org: <http://www.w3.org/ns/org#>
+    PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
+    PREFIX mandaat: <http://data.vlaanderen.be/ns/mandaat#>
+    PREFIX person: <http://www.w3.org/ns/person#>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+
+    SELECT ?email WHERE {
+      ${sparqlEscapeTermValue(mandatarisUri)} a mandaat:Mandataris;
+        org:holds ?mandaat.
+
+      ?bestuurseenheid a besluit:Bestuurseenheid.
+      ?bestuursorgaan besluit:bestuurt ?bestuurseenheid .
+      ?bestuursorgaanInTijd mandaat:isTijdspecialisatieVan ?bestuursorgaan .
+      ?bestuursorgaanInTijd org:hasPost ?mandaat .
+
+      OPTIONAL {
+        ?bestuurseenheid ext:mailAdresVoorNotificaties ?email.
+      }
+    } LIMIT 1
+  `;
+  const sparqlResult = await querySudo(query);
+  console.log(sparqlResult);
+  const result = findFirstSparqlResult(sparqlResult);
+
+  return result?.email;
 }
