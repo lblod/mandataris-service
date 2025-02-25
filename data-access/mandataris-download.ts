b204565e23d912165b677b02488c43c8ea1ef5e5
Ben Fidlers <ben.fidlers@qmino.com>
2025-02-25 12:17:20 +0100

    fix active filter download

--- data-access/mandataris-download.ts
+++ data-access/mandataris-download.ts
@@ -31,24 +31,13 @@ async function getUrisForFilters(filters) {
     `;
   }
   if (filters.activeOnly) {
-    const escapedTodaysDate = sparqlEscapeDateTime(new Date());
-    // the str() call is necessary because otherwise the comparison fails in virtuoso (datatype issue?)
     onlyActiveFilter = `
       OPTIONAL {
         ?mandataris mandaat:einde ?einde.
       }
-      ?bestuursorgaanInTijd mandaat:bindingStart ?startBestuursorgaan.
-      OPTIONAL {
-        ?bestuursorgaanInTijd mandaat:bindingEinde ?eindeBestuursorgaan.
-      }
-
-      BIND(IF(BOUND(?eindeBestuursorgaan), ?eindeBestuursorgaan, ${escapedTodaysDate}) AS ?safeEindeBestuursorgaan).
-      BIND(IF(BOUND(?einde), ?einde, ${escapedTodaysDate}) AS ?safeEinde).
-      FILTER (
-        STR(?safeEinde) <= STR(?safeEindeBestuursorgaan) &&
-        STR(?safeEinde) >= STR(?startBestuursorgaan) &&
-        STR(?safeEinde) >= STR(${escapedTodaysDate})
-      )
+      BIND(IF(BOUND(?einde), ?einde, "3000-01-01"^^xsd:dateTime) AS ?safeEinde)
+      BIND(NOW() AS ?now)
+      FILTER (?now <= ?safeEinde)
     `;
   }
 
