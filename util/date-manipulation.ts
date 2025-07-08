def7c9c2f8e71ea3eb47bdf85ba5e1b2239c6c85
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-07-08 14:45:58 +0200

    lmb-1675: change dates to use start and endOfDay date-manipulation value

--- util/date-manipulation.ts
+++ util/date-manipulation.ts
@@ -1,22 +1,32 @@
 import moment from 'moment';
 
-export function endOfDay(date?: Date) {
+export function endOfDay(date?: Date, returnNullWhenUndefined = false) {
+  if (!date && returnNullWhenUndefined) {
+    return null;
+  }
+
   if (date) {
     return moment(date)
       .add(1, 'days')
       .startOf('day')
       .subtract(1, 'second')
+      .utc()
       .toDate();
   } else {
     return moment()
       .add(1, 'days')
       .startOf('day')
       .subtract(1, 'second')
+      .utc()
       .toDate();
   }
 }
 
-export function startOfDay(date?: Date) {
+export function startOfDay(date?: Date, returnNullWhenUndefined = false) {
+  if (!date && returnNullWhenUndefined) {
+    return null;
+  }
+
   if (date) {
     return moment(date).startOf('day').utc().toDate();
   } else {
